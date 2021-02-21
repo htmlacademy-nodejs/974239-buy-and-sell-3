a91e701d521052a7e767902a5b8e97ac48e5f262
Гриневич Александр Васильевич (4029681) <grinevich@dev.vtb.ru>
2021-02-21 12:26:31 +0300

    module3-task2 Шаблоны для страниц
    Пока что возникает ошибка, что сервер не может найти изображения
    по путям, которые содержат два слеша
    
    Например, http://localhost:8080/my/comments
    тогда сервер будет пытаться найти изображения по пути
    localhost:8080/my/img/**

--- src/express/index.js
+++ src/express/index.js
@@ -2,17 +2,25 @@
 
 const express = require(`express`);
 const chalk = require(`chalk`);
+const path = require(`path`);
 
 const myRouter = require(`./routes/my`);
 const offersRouter = require(`./routes/offers`);
 const mainRouter = require(`./routes/main`);
 
 const PORT = 8080;
+const PUBLIC_DIR = `public`;
+
 const app = express();
 
-app.use(`/`, mainRouter);
-app.use(`/my`, myRouter);
 app.use(`/offers`, offersRouter);
+app.use(`/my`, myRouter);
+app.use(`/`, mainRouter);
+
+app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
+
+app.set(`views`, path.resolve(__dirname, `templates`));
+app.set(`view engine`, `pug`);
 
 app.listen(PORT, () => console.log(chalk.green(`Сервер запущен на порту ${PORT}`)));
 
